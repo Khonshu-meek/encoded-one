c0c984afa66d70f6f1c18ee9968de395f931fd48
Khonshu-meek <hasindumaddawatta@gmail.com>
2025-12-23 18:35:20 +0530

    Remove deprecated mongoose options

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('Mongo DB connection error:', err));
 
